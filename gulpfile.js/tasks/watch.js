3decbc944f7ef440c11ee25e4a50de375b2f6e8c
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2016-11-25 10:17:58 +0100

    Use gulp-watch for better performance on macOS

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -1,6 +1,7 @@
 // ==== WATCH ==== //
 
-var gulp        = require('gulp')
+var gulp = require('gulp')
+  , watch = require('gulp-watch')  
   , plugins     = require('gulp-load-plugins')({ camelize: true })
   , config      = require('../../gulpconfig').watch
 ;
@@ -8,10 +9,18 @@ var gulp        = require('gulp')
 // Watch (BrowserSync version): build stuff when source files are modified, let BrowserSync figure out when to reload
 // Task chain: build -> browsersync -> watch
 gulp.task('watch-browsersync', ['browsersync'], function() {
-  gulp.watch(config.src.styles, ['styles']);
-  gulp.watch(config.src.scripts, ['scripts']);
-  gulp.watch(config.src.images, ['images']);
-  gulp.watch(config.src.theme, ['theme']);
+  watch(config.src.styles, { usePolling: true, interval: 2000 }, function(file) { 
+      gulp.start('styles') 
+  }); 
+  watch(config.src.scripts, { usePolling: true, interval: 2000 }, function(file) { 
+      gulp.start('scripts') 
+  }); 
+  watch(config.src.images, { usePolling: true, interval: 2000 }, function(file) { 
+      gulp.start('images') 
+  }); 
+  watch(config.src.theme, { usePolling: true, interval: 2000 }, function(file) { 
+      gulp.start('theme') 
+  }); 
 });
 
 // Watch (Livereload version): build stuff when source files are modified, inform livereload when anything in the `build` or `dist` folders change
